f662cfce136ae57cb45fb327a2792f24287aafa8
Mikich Esaian <woknsushi@yandex.ru>
2022-04-20 20:06:16 +0300

    sec commit

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ import { ScrollTrigger} from "./node_modules/gsap/all"
 gsap.registerPlugin(ScrollTrigger)
 
 window.addEventListener('load', () => {
-    [...document.querySelector('#Gradient').children].forEach(stop => {
-        gsap.to(stop, {
-            stopColor: 'hsla(359, 50%, 60%, 100%)',
-            scrollTrigger: {
-                start: 0,
-                scrub: 1,
-                end: "max"
-            },
-        })
-    });
+    const stops = [...document.querySelector('#Gradient').children]
+    gsap.to(stops[0], {
+        stopColor: 'hsla(54, 89%, 62%, 92%)',
+        scrollTrigger: {
+            start: 0,
+            scrub: 1,
+            end: "max"
+        },
+    })
+    gsap.to(stops[1], {
+        stopColor: 'hsla(4, 100%, 50%, 100%)',
+        scrollTrigger: {
+            start: 0,
+            scrub: 1,
+            end: "max"
+        },
+    })
 })
